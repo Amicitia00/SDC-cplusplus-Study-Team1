9893864cc3272683fe79b4cd8a7b56d11527c9bc
Amicitia00 <wjh324@naver.com>
2023-12-08 17:47:29 +0900

    feat:[우정훈]request_form 미완성 [SDCT1-ACCOUNT-7]

--- Board/account/service/request/AccountRequest.h
+++ Board/account/service/request/AccountRequest.h
@@ -5,4 +5,19 @@
 #ifndef CLASS_TEST_ACCOUNTREQUEST_H
 #define CLASS_TEST_ACCOUNTREQUEST_H
 
+#include <string>
+#include "../../entity/Account.h"
+
+class AccountLoginRequest {
+private:
+    std::string account_id;
+    std::string password;
+
+public:
+    AccountLoginRequest(const std::string& account_id, const std::string& password);
+    Account *toAccount();
+    std::string getAccountId() const;
+    std::string getPassword() const;
+};
+
 #endif //CLASS_TEST_ACCOUNTREQUEST_H
